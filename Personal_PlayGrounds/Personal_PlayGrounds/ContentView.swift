804dcb0e74acd78bac75bb0576bc7978ff35f3d2
UNDaniel <niyejunze.j@gmail.com>
2020-02-22 17:55:47 +0800

    Initial projects

---
+++ Personal_PlayGrounds/Personal_PlayGrounds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Personal_PlayGrounds
+//
+//  Created by UNDaniel on 22/2/20.
+//  Copyright © 2020 UNDaniel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
